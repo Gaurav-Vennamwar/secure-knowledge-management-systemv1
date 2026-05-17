ca4a40c411418733f2525073e0df954c07e357de
Gaurav-Vennamwar <vennamwarg@gmail.com>
2026-05-17 21:26:07 +0530

    added the category table which will fetch all the data from data base

--- src/app/Features/Category/category-list/category-list.ts
+++ src/app/Features/Category/category-list/category-list.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CategoryService } from '../Services/category-service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,4 +8,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './category-list.html',
   styleUrl: './category-list.css',
 })
-export class CategoryList {}
+export class CategoryList {
+  private categoryService = inject(CategoryService);
+  private getAllCategoriesRef = this.categoryService.getAllCategories();
+   
+  //showing the user that we are still fetching the data directly using in html
+  isLoading = this.getAllCategoriesRef.isLoading;
+  isError = this.getAllCategoriesRef.error;
+  value = this.getAllCategoriesRef.value;
+}
