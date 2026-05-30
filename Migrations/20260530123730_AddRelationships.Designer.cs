1da0dd2836b6a5a9714d91dbddd1bb39224349a3
Gaurav-Vennamwar <vennamwarg@gmail.com>
2026-05-30 18:37:50 +0530

    created ef core relations between domain models

--- Migrations/20260530123730_AddRelationships.Designer.cs
+++ Migrations/20260530123730_AddRelationships.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SecureKnowledgeManagementSystemv1.API.Data;
 
@@ -11,9 +12,11 @@ using SecureKnowledgeManagementSystemv1.API.Data;
 namespace SecureKnowledgeManagementSystemv1.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260530123730_AddRelationships")]
+    partial class AddRelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260530123730_AddRelationships.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecureKnowledgeManagementSystemv1.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogPostCategory",
+                columns: table => new
+                {
+                    BlogPostsid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Categoriesid = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPostCategory", x => new { x.BlogPostsid, x.Categoriesid });
+                    table.ForeignKey(
+                        name: "FK_BlogPostCategory_BlogPosts_BlogPostsid",
+                        column: x => x.BlogPostsid,
+                        principalTable: "BlogPosts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlogPostCategory_Categories_Categoriesid",
+                        column: x => x.Categoriesid,
+                        principalTable: "Categories",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPostCategory_Categoriesid",
+                table: "BlogPostCategory",
+                column: "Categoriesid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogPostCategory");
+        }
+    }
+}
