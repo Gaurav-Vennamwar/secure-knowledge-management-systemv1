50a43f8d18bb7c28aa5d4e86c5af1a51c2d61e7d
Gaurav-Vennamwar <vennamwarg@gmail.com>
2026-06-11 20:56:02 +0530

    implemented the logout in ui

---
+++ src/app/Features/Auth/models/auth.model.ts
@@ -0,0 +1,12 @@
+export interface LoginResponse{
+    
+    Email : string;
+    Roles : string[];
+    token: string;
+}
+
+export interface User {
+    Email : string;
+    Roles : string[];
+    token: string;
+}
